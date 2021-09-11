926357bb1fc42755fd2315ea480f3b50c79ae585
Sachin <sacharora99@gmail.com>
2021-09-11 06:44:07 +0530

    Third Commit

---
+++ src/main/java/com/example/demo/MyDockerProj1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDockerProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDockerProj1Application.class, args);
+	}
+
+}
